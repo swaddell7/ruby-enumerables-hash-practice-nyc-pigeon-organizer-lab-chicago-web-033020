894417a913daf1dba0d2e7c9ce27fc6c121f8b92
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-20 20:35:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,13 @@
 def nyc_pigeon_organizer(data)
   final = {}
   data.each do |first_level, all_other|
-    all_other.each do |category
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+  
     
   end
 end
