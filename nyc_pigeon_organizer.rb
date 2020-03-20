83cde4ddcf453140c7d5a9aa8b376d2c649215c2
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-20 20:41:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,8 @@ def nyc_pigeon_organizer(data)
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
       x.each do |item|
-        
+        if bird_name === item
+          final[item][:color] << 
     
   end
 end
